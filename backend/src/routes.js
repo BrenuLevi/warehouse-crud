9a7360a1dd514b65aaeedd1f160ff2193d7ed0bb
Breno Soares <brenosoares4303@gmail.com>
2022-06-19 12:48:01 -0300

    Routes.js update

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,11 +5,12 @@ const router = Router();
 const CreateProductController = require("./controllers/CreateProductController");
 const FindProductController = require("./controllers/FindProductController");
 const EditProductController = require("./controllers/EditProductController");
+const DeleteProductController = require("./controllers/DeleteProductController");
 
 router.post("/create", CreateProductController.create);
 router.get("/list", FindProductController.show)
 router.get("/list/:filter", FindProductController.filtered);
 router.put("/edit/:id", EditProductController.edit);
-// router.delete("/delete/:id", DeleteProductController.delete);
+router.delete("/delete/:id", DeleteProductController.delete);
 
 module.exports = router;
